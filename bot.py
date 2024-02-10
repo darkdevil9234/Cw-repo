74547cc30eb90da7beaf57d93a37c5a1a986ac58
darkdevil9234 <130627650+darkdevil9234@users.noreply.github.com>
2024-02-10 16:50:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,3 +5,15 @@ import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 from bot import botStartTime
 
+
+def hrb(value, digits= 2, delim= "", postfix=""):
+    """Return a human-readable file size.
+    """
+    if value is None:
+        return None
+    chosen_unit = "B"
+    for unit in ("KiB", "MiB", "GiB", "TiB"):
+        if value > 1000:
+            value /= 1024
+            chosen_unit = unit
+        else:
