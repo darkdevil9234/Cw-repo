0dbfb226aaf18fb47be8f55f5b907eaaa76bed31
darkdevil9234 <130627650+darkdevil9234@users.noreply.github.com>
2024-02-10 16:34:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,15 +4,5 @@ from pyrogram import types
 import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
-
 app = Bot()
 app.run()
