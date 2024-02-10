24c5ad73c4d9edbf7eed7736e1b18b801dcb3e33
darkdevil9234 <130627650+darkdevil9234@users.noreply.github.com>
2024-02-10 15:39:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-from pyrogram import Client, __version__
-from web import web_app
+from pyrogram import Client, __version_
 from info import BOT_TOKEN API_ID, API_HASH
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
